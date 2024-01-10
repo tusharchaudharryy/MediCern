eb62f96c89d75a6b5418b3c1930fbfdd06557c7d
tusharchaudharryy <150551769+tusharchaudharryy@users.noreply.github.com>
2024-01-10 18:13:08 +0530

    Controllers updated

--- server/controllers/payments.js
+++ server/controllers/payments.js
@@ -10,7 +10,7 @@ const getPayments = async (req, res) => {
       console.log(error);
       res.status(400).send({ error: "Something went wrong" });
     }
-  }
+  } 
 
 const addPayment = async (req, res) => {
     const payload = req.body;
